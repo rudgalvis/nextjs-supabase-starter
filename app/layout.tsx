00df5b379f20375e96310bbefdca11c435c5386a
Rokas Rudgalvis <rokas@rudgalvis.com>
2025-12-14 13:56:17 +0200

    initial shadcn setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono, Inter } from "next/font/google"
 import "./globals.css"
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -22,7 +24,7 @@ const RootLayout = ({
 }: Readonly<{
     children: React.ReactNode
 }>) => (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
             {children}
         </body>
